4774393c7f788594aae99cb137253d635a1c1bab
Felippe Andrade <unchain0@proton.me>
2025-12-09 14:17:33 -0300

    style: fix closure formatting to match Laravel coding standards

--- desafio-3/routes/web.php
+++ desafio-3/routes/web.php
@@ -3,6 +3,6 @@
 use App\Livewire\TaskManager;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => redirect()->route('tasks.index'));
+Route::get('/', fn () => redirect()->route('tasks.index'));
 
 Route::get('/tasks', TaskManager::class)->name('tasks.index');
