71ae5dd3f0cd9b00b4e309cf24fe732a5b4c94c4
Felippe Andrade <unchain0@proton.me>
2025-12-09 21:11:53 -0300

    refactor(view): improve empty state component structure

--- desafio-3/resources/views/components/empty-state.blade.php
+++ desafio-3/resources/views/components/empty-state.blade.php
@@ -6,11 +6,15 @@
 
 <div class="px-6 py-12 text-center">
     <div class="text-gray-400 text-5xl mb-4">{{ $icon }}</div>
+
     @if ($title)
-        <p class="text-gray-500">{{ $title }}</p>
+        <h3 class="text-gray-500 text-lg font-medium">{{ $title }}</h3>
     @endif
+
     @if ($subtitle)
-        <p class="text-gray-400 text-sm mt-1">{{ $subtitle }}</p>
+        <p class="text-gray-400 text-sm mt-2">{{ $subtitle }}</p>
     @endif
-    {{ $slot }}
+    <div class="mt-4">
+        {{ $slot }}
+    </div>
 </div>
