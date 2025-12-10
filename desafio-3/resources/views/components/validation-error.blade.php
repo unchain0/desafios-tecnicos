c5f317fecfaf6a2b4e88cdaeaeae86276a424e37
Felippe Andrade <unchain0@proton.me>
2025-12-09 21:13:38 -0300

    refactor(view): improve validation error accessibility

--- desafio-3/resources/views/components/validation-error.blade.php
+++ desafio-3/resources/views/components/validation-error.blade.php
@@ -1,7 +1,11 @@
 @props(['field'])
 
 @error($field)
-    <div {{ $attributes->merge(['class' => 'mb-6 p-4 border rounded-lg bg-red-100 border-red-400 text-red-700']) }}>
-        {{ $message }}
+    <div
+        {{ $attributes->merge(['class' => 'mb-6 p-4 border rounded-lg bg-red-100 border-red-400 text-red-700 flex items-start']) }}
+        role="alert"
+    >
+        <span class="mr-2">⚠️</span>
+        <span>{{ $message }}</span>
     </div>
 @enderror
