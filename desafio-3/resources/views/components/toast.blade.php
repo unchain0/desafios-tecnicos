18f3d72b3e08c959867e88ad5c05fa7660dfbead
Felippe Andrade <unchain0@proton.me>
2025-12-09 21:13:17 -0300

    refactor(view): improve toast notification accessibility

--- desafio-3/resources/views/components/toast.blade.php
+++ desafio-3/resources/views/components/toast.blade.php
@@ -13,12 +13,28 @@
 @endphp
 
 @if ($message)
-    <div x-data="{ show: true }" x-init="setTimeout(() => { show = false;
-        $wire.clearMessage() }, {{ $duration }})" x-show="show"
-        x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 -translate-y-2"
-        x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in duration-150"
-        x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 -translate-y-2"
-        {{ $attributes->merge(['class' => "mb-6 p-4 border rounded-lg {$classes}"]) }}>
-        {{ $message }}
+    <div
+        x-data="{ show: true }"
+        x-init="setTimeout(() => { show = false; $wire.clearMessage(); }, {{ $duration }})"
+        x-show="show"
+        x-transition:enter="transition ease-out duration-200"
+        x-transition:enter-start="opacity-0 -translate-y-2"
+        x-transition:enter-end="opacity-100 translate-y-0"
+        x-transition:leave="transition ease-in duration-150"
+        x-transition:leave-start="opacity-100 translate-y-0"
+        x-transition:leave-end="opacity-0 -translate-y-2"
+        role="alert"
+        {{ $attributes->merge(['class' => "mb-6 p-4 border rounded-lg {$classes}"]) }}
+    >
+        <div class="flex items-start">
+            <span class="mr-2">
+                @if ($type === 'success')
+                    ✅
+                @else
+                    ⚠️
+                @endif
+            </span>
+            <span>{{ $message }}</span>
+        </div>
     </div>
 @endif
