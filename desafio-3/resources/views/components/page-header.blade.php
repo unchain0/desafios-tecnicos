bacd7c35a99d8046e1c3eca8864d8e33cc4d1892
Felippe Andrade <unchain0@proton.me>
2025-12-09 21:12:24 -0300

    refactor(view): improve page header with semantic HTML

--- desafio-3/resources/views/components/page-header.blade.php
+++ desafio-3/resources/views/components/page-header.blade.php
@@ -4,13 +4,15 @@
     'subtitle' => '',
 ])
 
-<div class="mb-8">
-    <h1 class="text-3xl font-bold text-gray-800 mb-2">
+<header class="mb-8">
+    <h1 class="text-3xl font-bold text-gray-800 mb-2 flex items-center gap-2">
         @if ($icon)
-            {{ $icon }}
-        @endif{{ $title }}
+            <span>{{ $icon }}</span>
+        @endif
+        <span>{{ $title }}</span>
     </h1>
+
     @if ($subtitle)
-        <p class="text-gray-600">{{ $subtitle }}</p>
+        <p class="text-gray-600 mt-1">{{ $subtitle }}</p>
     @endif
-</div>
+</header>
